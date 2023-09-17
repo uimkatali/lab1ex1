a8e47ceafb478473abb955e670d1e8e942723bd5
uimkatali <b8bf6b5@gmail.com>
2023-09-17 20:49:34 +0300

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float length, width, height;
+            length = Convert.ToInt32(Console.ReadLine());
+            width = Convert.ToInt32(Console.ReadLine());
+            height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(width * height * length);
+        }
+    }
+}
